0f3c61988db280e886ea3318630c1f84bd6f417e
ahrian87 <maciej.zbydniewski@gmail.com>
2022-11-07 20:35:18 +0100

    Banking App - added comments and some minor tweaks

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -19,39 +19,47 @@ public class Account {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
-
+    // zwraca login
     public String getLogin() {
         return login;
     }
 
+    // zwraca pin
     public int getPin() {
         return pin;
     }
 
+    // zwraca pierwsze imie klienta
     public String getFirstName() {
         return firstName;
     }
 
+    // zwraca nazwisko klienta
     public String getLastName() {
         return lastName;
     }
 
+    // zwraca pesel klienta
     public String getPesel() {
         return pesel;
     }
 
+    // zwraca numer konta klienta
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    // zwraca stan konta klienta
     public double getBalance() {
         return balance;
     }
 
+    // dodaje podana kwotę (amount) do stanu konta klienta
     public void addBalance(double amount) {
         this.balance = balance + amount;
     }
 
+    // odejmuje podaną kwotę (amount) od stanu konta klienta
     public void subtractBalance(double amount) {
         this.balance = balance - amount;
     }
